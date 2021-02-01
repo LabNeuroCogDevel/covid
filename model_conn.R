0307f11609a30a3be0f355d02eae976f97aecedf
Will Foran <willforan@gmail.com>
2021-02-01 15:33:57 -0500

    ✨ model is a parameter; rename harmonize_and_model

--- model_conn.R
+++ model_conn.R
@@ -1,10 +1,13 @@
 require(neuroCombat) 
+require(lme4)
 
 # treat data as global variable in functions
+# this is a large (~180Mb) file
 CONNDATA <- read.csv('data/covid_conn_long.csv')
 
 
-# from FC via MP
+# narrow to large DF to just the connection we want to inspect
+# connName like "NAcc_Caudate"
 subset_conn <- function(connName){
   this <- CONNDATA[CONNDATA$connName == connName,]
   this <- this[this$age >0,]
@@ -13,6 +16,7 @@ subset_conn <- function(connName){
 
 # make 'conn' harnomized value, 'conn.original' has old values
 # also subset to zscore(conn) < 3
+# from FC via MP
 harmonize <- function(connName) {
   this <- subset_conn(connName)
   batch = as.factor(c(this$study))
@@ -36,8 +40,11 @@ harmonize <- function(connName) {
   this <- this[which(abs(z) < 3),]
 }
 
-model_conn <- function(harmonized) {
-  model <- conn ~ age + sex + fd_mean + srcHemi + lat + (1|study/subj)
+model_conn <- function(harmonized, model=NULL) {
+  # default model
+  if(is.null(model))
+    model <- conn ~ age + sex + fd_mean + srcHemi + lat + (1|study/subj)
+
   m <- lmer(data=harmonized, model)
 }
 
@@ -70,8 +77,8 @@ plot_model_sexcolor <- function(m) {
       ggtitle(glue::glue("{conn} by age w/sex"))
 }
 
-model_harmonize <-function(connName){
-   tryCatch(model_conn(harmonize(connName)),error=function(e) NULL)
+harmonize_and_model <-function(connName, ...){
+   tryCatch(model_conn(harmonize(connName), ...),error=function(e) NULL)
 }
 
 # get age pvalue from model
@@ -79,10 +86,11 @@ pval_age <- function(m) summary(m)$coefficients['age','Pr(>|t|)']
 age_issig <- function(m, p=.05) pval_age(m) < p
 
 ### START HERE
-model_CONNDATA <-function(){
-
+model_CONNDATA <-function(model=NULL){
+    # if model is null, default set in model_conn is used
+    #  model <- conn ~ age + sex + fd_mean + srcHemi + lat + (1|study/subj)
     all_conns <- unique(CONNDATA$connName)
-    all_models <- lapply(all_conns, model_harmonize)
+    all_models <- lapply(all_conns, harmonize_and_model, model=model)
     names(all_models) <- all_conns
 
     # remove models that failed
@@ -93,6 +101,7 @@ model_CONNDATA <-function(){
     summary(all_pvals)
     # Min.     1st Qu.    Median      Mean   3rd Qu.      Max.
     #0.0000156 0.2011451 0.4555715 0.4596300 0.7302006 0.9766166
+
     names(all_pvals[all_pvals <= .05])
     # "NAcc_amOFC1"    "NAcc_avmPFC"    "NAcc_sgACC"     "NAcc_rACC"     
     # "Caudate_avmPFC" "Caudate_A32p"   "Caudate_A24cd"  "Caudate_dIa"   
@@ -100,16 +109,19 @@ model_CONNDATA <-function(){
     # "amOFC1_amOFC2"  "avmPFC_rACC"   
     
 
+    # could use base::Filter (not dpylyr:filter) to retrict. but we already have
+    # all the pavlues, so this is redudant 
     #sig_models <- Filter(function(m) age_issig(m, p=.01), all_models)
-    sig_models_05 <- all_models[all_pvals <= .05]
-    cat(glue::glue("{length(all_conns)} total connections, {length(all_models)} modeled, and {length(sig_models_05)} (.05) with sig age effect (uncorrected)\n"))
 
+    sig_models_05 <- all_models[all_pvals <= .05]
+    cat(glue::glue("{length(all_conns)} total connections, {length(all_models)} modeled, and {length(sig_models_05)} with sig age effect (p<.05, uncorrected)\n"))
 
     
     plot_conn(names(sig_models_05)[1])
     plot_model_sexcolor(sig_models_05[[1]])
 
-    return(sig_models_05)
+    summary(all_models$NAcc_amOFC1)
 
+    return(sig_models_05)
 }
 
