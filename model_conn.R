bafa1a0b6fd07e53362f7ee374450c52801e6776
WillForan <willforan@gmail.com>
2021-05-12 21:16:45 -0400

    💡 model_conn.R: add filter_sig_models; detailed comments everywhere

--- model_conn.R
+++ model_conn.R
@@ -1,12 +1,34 @@
 #!/usr/bin/env Rscript
+# model_conn.R
+# 20210512 WF/OR - rework for composite (lm, not mixed effects)
+#                  model:  conn ~ composite + age + sex + fd_mean + study
+# 20210203       - model: conn ~ age + sex + fd_mean + srcHemi + lat + (1|study/subj)
+#
+#
+# USAGE:
+#
+#  all_models <- model_CONNDATA()
+#  sig_models <- filter_sig_models(all_models, 'composite', .05)
+# 
+#  where model_CONNDATA() puts most of the smaller functions together
+#  and filter_sig_models() limits the list of models to those that are interesting
+#  see debug_me() for specifics
+#  
+# NOTES:
+#  CONNDATA is very large. some effort is taken to reduce frequently loading it
+#    burn-your-eyes bad coding practice: CONNDATA (and CONNDATA_orig) are globals!
+#    using '<<-' assignment operator to set 
+#
+# large files are not tracked in git. expect data/ to have
+#  data/covid_composite_0321.csv
+#  data/covidv3_conn_long.csv
+# these are on rhea:/Volumes/Hera/Projects/covid/data
+
 require(neuroCombat) 
 require(lme4)
 require(dplyr)
 require(ggplot2)
 require(tidyr)
-require(neuroCombat) 
-require(lme4)
-require(dplyr)
 require(glue)
 
 # treat data as global variable in functions
@@ -15,7 +37,8 @@ require(glue)
 CONNDATA <- NULL      # data with e.g. hemi's collapsed and anxitey score merged
 CONNDATA_orig <- NULL # raw 180Mb csv file
 
-# DEBUG
+#' step through other functions
+#' run this line by line to find any breaking changes
 debug_me <- function() {
     print("hows the data look")
     set_CONNDATA()
@@ -35,6 +58,10 @@ debug_me <- function() {
 
     print("model harm conn")
     print(summary(model_conn(hmz)))
+
+    # DONT RUN HERE - running for all connName will take some time
+    # all_models <- model_CONNDATA()
+    # sig_models <- filter_sig_models(all_models, 'composite', .05)
 }
 
 # group by all the the columns we might use later
@@ -98,8 +125,9 @@ CONNDATA_info <- function() {
 }
 
 
-# narrow large df to just the connection we want to inspect
-# @param thisConnName roi-roi pair connectivity name "NAcc_Caudate"
+#' narrow large df to just the connection we want to inspect
+#' @param thisConnName roi-roi pair connectivity name "NAcc_Caudate"
+#' used by harmonize() and plot_conn()
 subset_conn <- function(thisConnName){
   CONNDATA %>%
     filter(connName == thisConnName,
@@ -107,9 +135,12 @@ subset_conn <- function(thisConnName){
            !is.na(conn))
 }
 
-# make 'conn' harnomized value, 'conn.original' has old values
+#' neruocombat's harmonize to "remove" study for a single connection
+#' make 'conn' column harnomized value, 'conn.original' has old values
+#' data.harmonized3 == conn, naming held over from original code (FC via MP, 2021-02)
+#' @param connName - connection name in CONNDATA 
 # also subset to zscore(conn) < 3
-# from FC via MP
+# output of this function is dataframe input to model_conn()
 harmonize <- function(connName) {
   this <- subset_conn(connName)
   batch = as.factor(c(this$study))
@@ -146,6 +177,11 @@ default_model <- function(d) {
     model <- conn ~ composite + age + sex + fd_mean + study
 }
 
+#' model a single connection's study harmonized dataframe
+#' model function is lm. but if we find 'subj' in model, use lmer
+#' @param harmonized dataframe from harmonize() function
+#' @param model model to use. NULL default uses value returned by default_model()
+#' input is from harmonized, output goes into list created by model_CONNDATA
 model_conn <- function(harmonized, model=NULL) {
   # default to ... the default model
   if(is.null(model)) model <- default_model(harmonized)
@@ -202,27 +238,16 @@ harmonize_and_model <-function(connName, ...){
     NULL})
 }
 
-# get age pvalue from model
-#pval_age <- function(m) summary(m)$coefficients['age','Pr(>|t|)']
-pval_composite <- function(m) summary(m)$coefficients['composite','Pr(>|t|)']
-#age_issig <- function(m, p=.05) pval_age(m) < p
-composite_issig <- function(m, p=.05) pval_composite(m) < p
+# summary coeff contain pvalue for lm model
+pvalue_of    <- function(m, column) summary(m)$coefficients[column,'Pr(>|t|)']
+column_issig <- function(m, column, p=.05) pvalue_of(m, column) < p
 
-### START HERE
-#' harmonize and lm or lmer for each connection
-#' @param model model to use. e.g. conn ~ composite + age + sex + fd_mean + study
-#'              if null, get from using default_model() on harmonized output
-model_harmonize_all <- function(model=NULL){
-  # updating global variable! only do it if it doesn't exist
-  if(!exists("CONNDATA") || is.null(CONNDATA)) set_CONNDATA()
-  
-  # if model is null, default set in model_conn is used
-  #  model <- conn ~ age + sex + fd_mean + srcHemi + lat + (1|study/subj)
-  all_conns <- unique(CONNDATA$connName)
-  all_models <- lapply(all_conns, harmonize_and_model, model=model)
-  names(all_models) <- all_conns
-  return(all_models)
-}
+# get age pvalue from model
+pval_age  <- function(m) pvalue_of(m, 'age')
+age_issig <- function(m, p=.05) pvalue_of(m,'age') < p
+
+composite_issig <- function(m, p=.05) pvalue_of(m, 'composite') < p
+
 
 #' debug a model for a given connection
 #' @param lm/lmer model default NULL uses default_model() 
@@ -233,8 +258,11 @@ model_one <- function(model=NULL, conn=NULL) {
     harmonize_and_model(conn)
 }
 
-
-
+### START HERE
+#' main driver. harmonize and model each connName in global var CONNDATA
+#' @param model model to use. e.g. conn ~ composite + age + sex + fd_mean + study
+#'              if null, get from using default_model() on harmonized output
+#'see filter_sig_models() for next step
 model_CONNDATA <-function(model=NULL){
   # updating global variable! only do it if it doesn't exist
   if(!exists("CONNDATA") || is.null(CONNDATA)) set_CONNDATA()
@@ -248,13 +276,27 @@ model_CONNDATA <-function(model=NULL){
   # remove models that failed
   # N.B. names(all_models) is no longer the same as all_conns
   all_models <- Filter(function(x) !is.null(x), all_models)
-  
-  all_pvals <- sapply(all_models, pval_age)
-  summary(all_pvals)
+  cat(glue("have {length(all_conns)} connections and {length(all_models)} successful models\n"))
+  return(all_models)
+}
+
+#' filter results of model_CONNDATA() based on a modeled pvalue
+#' @param all_models output of model_CONNDATA()
+#' @param column the model input whos pvalue to filter on
+#' @param p      models will all have column's pvalue < p
+filter_sig_models <- function(all_models, column='composite', p=.05) {
+  sig_test <- function(m) pvalue_of(m, column)
+  all_pvals <- sapply(all_models, sig_test)
+  cat(glue("{column} pvalues dist:"),"\n")
+  print(summary(all_pvals))
+  # for original model (no anxiety composite score)
   # Min.     1st Qu.    Median      Mean   3rd Qu.      Max.
   #0.0000156 0.2011451 0.4555715 0.4596300 0.7302006 0.9766166
   
-  names(all_pvals[all_pvals <= .05])
+  model_is_sig <- all_pvals < p
+  cat(glue("{length(which(model_is_sig))}/{length(all_models)} connections where model's '{column}' pvalues < {p}  (uncorrected):"),"\n\t")
+  cat(paste(collapse=", ", names(all_pvals[model_is_sig])), "\n")
+  # originally:
   # "NAcc_amOFC1"    "NAcc_avmPFC"    "NAcc_sgACC"     "NAcc_rACC"     
   # "Caudate_avmPFC" "Caudate_A32p"   "Caudate_A24cd"  "Caudate_dIa"   
   # "Putamen_avmPFC" "Putamen_A24cd"  "aHPC_avmPFC"    "pHPC_pmOFC1"   
@@ -265,13 +307,11 @@ model_CONNDATA <-function(model=NULL){
   # all the pavlues, so this is redudant 
   #sig_models <- Filter(function(m) age_issig(m, p=.01), all_models)
   
-  sig_models_05 <- all_models[all_pvals <= .05]
-  cat(glue("{length(all_conns)} total connections, {length(all_models)} modeled, and {length(sig_models_05)} with sig composite effect (p<.05, uncorrected)\n"))
+  sig_models <- all_models[model_is_sig]
   
   
 #  plot_conn(names(sig_models_05)[1])
 #  plot_model_sexcolor(sig_models_05[[1]])
   
-  return(all_models)
-  return(sig_models_05)
+  return(sig_models)
 }
